46233986c0061581d29343a43518884f89a6b8d1
Bishal Chalise <bishalise@gmail.com>
2025-09-19 10:55:28 +0200

    completed

--- part2/country/src/services/country.js
+++ part2/country/src/services/country.js
@@ -2,8 +2,6 @@ import axios from "axios";
 const url = "https://studies.cs.helsinki.fi/restcountries/api";
 const api_key = import.meta.env.VITE_SOME_KEY;
 const weather_url = "https://api.openweathermap.org/data/2.5/weather";
-const apiforbrowser =
-  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=32b29903a827acae7d75c4c9b3b9edc5&units=metric";
 
 const getAllCountry = () => {
   const request = axios.get(`${url}/all`);
